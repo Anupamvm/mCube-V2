f14a8a285931ae5b55caa5a304c1a6e069c05162
Anupamvm <anupamvm@gmail.com>
2024-11-15 15:59:51 +0530

    Initial commit

---
+++ ksneo/ksapi.py
@@ -0,0 +1,9 @@
+#login - return status
+#logout - return status
+#savequote - Async call
+#placeorder - reuturn status
+#getpositions - return live positions 
+#updateholding - Update db - holding
+#updatewatchlist - Update db - watchlist
+#searchscript - return script token
+#updatescriptmasters - Update files
---
+++ research/fundamental.py
@@ -0,0 +1,13 @@
+#updateKeyEvent - Update DB
+#verifyKeyEvent5days - Returns true if any key event in 5 days (FedMeet, RBi meet, Election etc)
+#verifyKeyEvent30days
+#updateResultDates(token) - Update DB
+#verifyResultDate5Days(token) - Returns true if result is in 5 days
+#getNoticeNSE(token)
+#getNoticeBSE(token)
+#getBulkDealData30days(token)
+#getMFTransaction(token)
+#getCompititionNoticeNSE(token)
+#getCompititionNoticeBSE(token)
+#getCompititionBulkDealData30days(token)
+#getCompititionMFTransaction(token)
